898c4dc8207c1716cefa71572a26e05098e976c0
Anshul Gadia <anshulgadia04@gmail.com>
2026-04-08 22:50:14 +0530

    added url

--- loan-web/src/api/axios.js
+++ loan-web/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.DEV ? 'http://localhost:5000/api' : '/api',
+  baseURL: "https://loan-admin-web.vercel.app/api"
 });
 
 // Attach token to every request
